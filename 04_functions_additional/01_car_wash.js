308b3fb5582bd5dbf5933aeae7fb737030009665
Allyster <ovardovslav@gmail.com>
2025-04-21 17:06:11 +0300

    Adding aditional tasks from module "Functions"

---
+++ 04_functions_additional/01_car_wash.js
@@ -0,0 +1,23 @@
+function cleanCar(commandsArray){
+    let carClean = 0
+
+    for (let command of commandsArray) {
+        if(command === 'soap'){
+            carClean += 10
+        }
+        else if(command === 'water'){
+            carClean += carClean * 0.2
+        }
+        else if(command === 'vacuum cleaner'){
+            carClean += carClean * 0.25
+        }
+        else if(command === 'mud'){
+            carClean -= carClean * 0.1
+        }
+    }
+
+    console.log(`The car is ${carClean.toFixed(2)}% clean.`);
+    
+}
+
+cleanCar(['soap', 'soap', 'vacuum cleaner', 'mud', 'soap', 'water'])
---
+++ 04_functions_additional/02_number_moification.js
@@ -0,0 +1,25 @@
+function modifyNumber(number){
+    let numberString = number.toString()
+    let validAverage = false
+
+    while(!validAverage){
+        let result = 0
+
+        for (let element of numberString) {
+            result += Number(element)
+        }
+
+        if(result / numberString.length > 5){
+            validAverage = true
+        }
+        else {
+            numberString += '9'
+        }
+    }
+    
+    console.log(numberString);
+    
+}
+
+modifyNumber(101)
+modifyNumber(5835)
