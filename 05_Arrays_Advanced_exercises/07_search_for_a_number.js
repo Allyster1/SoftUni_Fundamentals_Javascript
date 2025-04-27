ebafe6fac2cb484900a53d37393a94bb55418099
Allyster <ovardovslav@gmail.com>
2025-04-27 23:17:16 +0300

    Create 07_search_for_a_number.js

---
+++ 05_Arrays_Advanced_exercises/07_search_for_a_number.js
@@ -0,0 +1,25 @@
+function findOccurances(array, numbersToFind) {
+    const [elementsToTake, deleteElements, searchElement] = numbersToFind
+    let counter = 0
+
+    let updatedArray = array.slice(0, elementsToTake)
+    let splicedArray = updatedArray.splice(0, deleteElements)
+
+    for (let element of updatedArray) {
+        if (element === searchElement) {
+            counter++
+        }
+    }
+
+    // for (const element of updatedArray) {
+    //     counter++
+    // }
+
+    console.log(`Number ${searchElement} occurs ${counter} times.`)
+}
+
+findOccurances([5, 2, 3, 4, 1, 6],
+    [5, 2, 3])
+
+findOccurances([7, 1, 5, 8, 2, 7],
+    [3, 1, 5])
